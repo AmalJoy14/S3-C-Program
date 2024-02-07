5b82acf2109f4d62bd681dc4194df6d076196f4d
Amal Joy <145443570+AmalJoy14@users.noreply.github.com>
2024-02-07 21:06:22 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,68 @@
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include<stdbool.h>
+
+int top = -1;
+char stack[20];
+
+void push(char element);
+char pop();
+int priority(char element);
+
+int main() {
+    push('(');
+    char infix[20];
+    printf("Enter the infix expression :\n");
+    scanf("%s", infix);
+    printf("\nIt's postfix expression is :\n");
+    for (int i = 0; infix[i] != '\0'; i++) {
+        if (isalnum(infix[i]))
+            printf("%c ", infix[i]);
+        else if (infix[i] == '(')
+            push(infix[i]);
+        else if (infix[i] == ')')
+            while (true){
+                if(pop() != '(')
+                    break;
+                }
+        else {
+            while (priority(stack[top]) >= priority(infix[i])) {
+                pop();
+            }
+            push(infix[i]);
+        }
+    }
+    while (top != -1) {
+        pop();
+    }
+}
+
+void push(char element) {
+    top++;
+    stack[top] = element;
+}
+
+char pop() {
+    char x;
+    x = stack[top];
+    if (x != '(')
+        printf("%c ", x);
+    top--;
+    return x;
+}
+
+int priority(char element) {
+    if (element == '^')
+        return 3;
+    else if (element == '*' || element == '/')
+        return 2;
+    else if (element == '+' || element == '-')
+        return 1;
+    else
+        return 0;
+}
+
+
+
